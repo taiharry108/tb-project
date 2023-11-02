44508f3110a8be9ffb1fd64ca960af037735a6ce
Harry Chan <taiharry108@gmail.com>
2023-11-02 13:21:42 -0700

    generate key and create network

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source .env
-
+make gen-key
+docker network create core-vn
 sed -e s/POSTGRES_USER/${POSTGRES_USER}/g -e s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g core-repo/src/alembic.ini.template > core-repo/src/alembic.ini
 sed -e s/POSTGRES_USER/${POSTGRES_USER}/g -e s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g ac-app/webapp/config.yml.template > ac-app/webapp/config.yml
 sed -e s/POSTGRES_USER/${POSTGRES_USER}/g -e s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g auth-app/webapp/config.yml.template > auth-app/webapp/config.yml
