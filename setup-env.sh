97a4dd29ad45ab3ee832b67b5c22fa8661c2e084
Harry Chan <taiharry108@gmail.com>
2024-03-08 16:27:29 -0700

    update setup script

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source .env
 make gen-key
-docker network create core-vn
+docker network create demo_net
 sed -e s/POSTGRES_USER/${POSTGRES_USER}/g -e s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g core-repo/src/alembic.ini.template > core-repo/src/alembic.ini
-sed -e s/POSTGRES_USER/${POSTGRES_USER}/g -e s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g ac-app/webapp/config.yml.template > ac-app/webapp/config.yml
-sed -e s/POSTGRES_USER/${POSTGRES_USER}/g -e s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g auth-app/webapp/config.yml.template > auth-app/webapp/config.yml
+cp ac-app/webapp/config.yml.template ac-app/webapp/config.yml
+cp auth-app/webapp/config.yml.template auth-app/webapp/config.yml
 
 mkdir -p core-repo/src/alembic/versions
